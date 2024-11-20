054b2d24cc7f66c6dfc8953bb0192ac39b336f78
Alejadro <alejandrogrpr2603@gmail.com>
2024-11-20 14:16:29 +0100

    Initial commit: created Java project Added table creation and display Initialized table with random values Sorted table elements

--- Task2/src/GitHubHomeworks/Main.java
+++ Task2/src/GitHubHomeworks/Main.java
@@ -1,14 +1,17 @@
 package GitHubHomeworks;
 
 import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
         int[] table = new int[5];
         Random rand = new Random();
         for (int i = 0; i < table.length; i++) {
-            table[i] = rand.nextInt(100); // Random values from 0-99
+            table[i] = rand.nextInt(100);
         }
+        Arrays.sort(table); // Sort table
         for (int i : table) {
             System.out.println(i);
         }
