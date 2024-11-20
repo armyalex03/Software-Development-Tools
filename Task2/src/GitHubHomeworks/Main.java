b53bcb65bb39edfde878231726e6f0fc32272271
Alejadro <alejandrogrpr2603@gmail.com>
2024-11-20 14:13:20 +0100

    Initial commit: created Java project Added table creation and display Initialized table with random values

--- Task2/src/GitHubHomeworks/Main.java
+++ Task2/src/GitHubHomeworks/Main.java
@@ -1,9 +1,14 @@
 package GitHubHomeworks;
 
-public class Main {
+import java.util.Random;
 
+public class Main {
     public static void main(String[] args) {
-        int[] table = {1, 2, 3, 4, 5};
+        int[] table = new int[5];
+        Random rand = new Random();
+        for (int i = 0; i < table.length; i++) {
+            table[i] = rand.nextInt(100); // Random values from 0-99
+        }
         for (int i : table) {
             System.out.println(i);
         }
