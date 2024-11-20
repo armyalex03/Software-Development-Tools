5846688281dc2b444f252dac92b2292ba3692238
Alejadro <alejandrogrpr2603@gmail.com>
2024-11-20 14:12:20 +0100

    Initial commit: created Java project Added table creation and display

--- Task2/src/GitHubHomeworks/Main.java
+++ Task2/src/GitHubHomeworks/Main.java
@@ -1,10 +1,11 @@
-
 package GitHubHomeworks;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        int[] table = {1, 2, 3, 4, 5};
+        for (int i : table) {
+            System.out.println(i);
+        }
     }
-    
 }
